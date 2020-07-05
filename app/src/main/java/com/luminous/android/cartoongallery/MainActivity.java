fd1fd8091f09ff9e1cdc12855617723e8b9991de
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-05 12:39:10 +0600

    image renamed

--- app/src/main/java/com/luminous/android/cartoongallery/MainActivity.java
+++ app/src/main/java/com/luminous/android/cartoongallery/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<List<String>> cartoonLists;
+    private List<Integer> categoryImageResources;
+    private Integer[] categoryImageArray = new Integer[] { R.drawable.bhoot,
+            R.drawable.thakumar_jhuli,
+            R.drawable.tuntuni,
+            R.drawable.rupkotha};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        categoryImageResources = Arrays.asList(categoryImageArray);
+
+//        List<String> bhootList = Arrays.asList(getResources().getStringArray(R.array.bhoot));
+//        cartoonLists.add(bhootList);
+
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.bhoot)));
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.thakumar_jhuli)));
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.tuntuni)));
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.rupkotha)));
+
     }
 }
