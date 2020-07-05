b9372bb81120393a716a7a0278287f46bbfebad7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-05 15:07:02 +0600

    content list

--- app/src/main/java/com/luminous/android/cartoongallery/CategoryRecyclerAdapter.java
+++ app/src/main/java/com/luminous/android/cartoongallery/CategoryRecyclerAdapter.java
@@ -1,10 +1,59 @@
 package com.luminous.android.cartoongallery;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 /**
  * Created by Shah Md Imran Hossain on 05, July, 2020
  */
 public class CategoryRecyclerAdapter extends RecyclerView.Adapter<CategoryRecyclerAdapter.ViewHolder> {
-    
+
+    private final Context context;
+    private final LayoutInflater layoutInflater;
+    private final List<Integer> categoryThumbnailResources;
+    private final List<List<String>> categoryLists;
+
+    public CategoryRecyclerAdapter(Context context, List<Integer> categoryThumbnailResources, List<List<String>> categoryLists) {
+        this.context = context;
+        layoutInflater = LayoutInflater.from(context);
+        this.categoryThumbnailResources = categoryThumbnailResources;
+        this.categoryLists = categoryLists;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = layoutInflater.inflate(R.layout.item_category_list, parent, false);
+
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.categoryThumbnail.setImageResource(categoryThumbnailResources.get(position));
+        holder.currentPosition = position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryLists.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public final ImageView categoryThumbnail;
+        public int currentPosition;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            categoryThumbnail = itemView.findViewById(R.id.categoryThumbnail);
+        }
+    }
 }
