7081d6cc57c28695f441c9662b5b435f9cd10593
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-05 18:40:26 +0600

    youtube player

--- app/src/main/java/com/luminous/android/cartoongallery/YoutubePlayer.java
+++ app/src/main/java/com/luminous/android/cartoongallery/YoutubePlayer.java
@@ -30,6 +30,18 @@ public class YoutubePlayer extends AppCompatActivity {
         });
     }
 
+     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        youTubePlayerView.release();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        hideSystemUI();
+    }
+
     private void hideSystemUI() {
         // Enables regular immersive mode.
         // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
