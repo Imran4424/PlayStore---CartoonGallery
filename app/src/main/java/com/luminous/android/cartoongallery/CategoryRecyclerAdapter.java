85a29e7de9191d89eb0d4c0edfcb89407eb1a1ff
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-05 13:08:01 +0600

    item category list

--- app/src/main/java/com/luminous/android/cartoongallery/CategoryRecyclerAdapter.java
+++ app/src/main/java/com/luminous/android/cartoongallery/CategoryRecyclerAdapter.java
@@ -1,7 +1,10 @@
 package com.luminous.android.cartoongallery;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 /**
  * Created by Shah Md Imran Hossain on 05, July, 2020
  */
-public class CategoryRecyclerAdapter {
+public class CategoryRecyclerAdapter extends RecyclerView.Adapter<CategoryRecyclerAdapter.ViewHolder> {
+    
 }
