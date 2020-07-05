d777d7707261fd4a35c043098c67c1535bbe3a2f
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-05 18:34:23 +0600

    youtube player

---
+++ app/src/main/java/com/luminous/android/cartoongallery/YoutubePlayer.java
@@ -0,0 +1,14 @@
+package com.luminous.android.cartoongallery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YoutubePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube_player);
+    }
+}
