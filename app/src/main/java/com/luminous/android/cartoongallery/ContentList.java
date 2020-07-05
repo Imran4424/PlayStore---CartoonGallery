3a53e374e0109deaafd8a97b9bca4c77bd54cd3d
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-07-05 17:35:17 +0600

    item content list

--- app/src/main/java/com/luminous/android/cartoongallery/ContentList.java
+++ app/src/main/java/com/luminous/android/cartoongallery/ContentList.java
@@ -4,12 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ContentList extends AppCompatActivity {
     public static int position;
+    List<List<String>> cartoonLists;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_list);
+
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.bhoot)));
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.thakumar_jhuli)));
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.tuntuni)));
+        cartoonLists.add(Arrays.asList(getResources().getStringArray(R.array.rupkotha)));
+
+        
     }
 }
