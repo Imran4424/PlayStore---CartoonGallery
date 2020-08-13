4331e62afc2fd4e1a6e407830a61a0582df1737a
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-08-13 09:58:58 +0600

    youtube player java file updated

--- app/src/main/java/com/luminous/android/cartoongallery/YoutubePlayer.java
+++ app/src/main/java/com/luminous/android/cartoongallery/YoutubePlayer.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -12,9 +13,10 @@ import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class YoutubePlayer extends AppCompatActivity {
+public class YoutubePlayer extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     public static String youtubeVideoId;
     private YouTubePlayerView youTubePlayerView;
+    private final String YOUTUBE_API_KEY = "AIzaSyDYHcgLlMAzSxYq1gdzGM0PUvleBO1zCME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,27 +24,8 @@ public class YoutubePlayer extends AppCompatActivity {
         setContentView(R.layout.activity_youtube_player);
 
         youTubePlayerView = findViewById(R.id.youtube_player_view);
+        youTubePlayerView.initialize(YOUTUBE_API_KEY, this);
 
-
-        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
-            @Override
-            public void onReady(@NonNull final YouTubePlayer youTubePlayer) {
-                 if (MainActivity.interstitialAd.isLoaded()) {
-                     MainActivity.interstitialAd.show();
-
-                     MainActivity.interstitialAd.setAdListener(new AdListener() {
-                        @Override
-                        public void onAdClosed() {
-                            youTubePlayer.loadVideo(youtubeVideoId, 0);
-                        }
-                     });
-                 } else {
-                        youTubePlayer.loadVideo(youtubeVideoId, 0);
-                 }
-
-                 MainActivity.loadInterstitialAd();
-            }
-        });
 
         hideSystemUI();
     }
@@ -78,5 +61,41 @@ public class YoutubePlayer extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, final YouTubePlayer youTubePlayer, boolean wasRestored) {
+        youTubePlayer.setFullscreen(true);
+        youTubePlayer.setShowFullscreenButton(false);
+        youTubePlayer.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+
+        if (!wasRestored) {
+            if (MainActivity.interstitialAd.isLoaded()) {
+                     MainActivity.interstitialAd.show();
+
+                     MainActivity.interstitialAd.setAdListener(new AdListener() {
+                        @Override
+                        public void onAdClosed() {
+                            youTubePlayer.loadVideo(youtubeVideoId);
+                        }
+                     });
+                 } else {
+                        youTubePlayer.loadVideo(youtubeVideoId);
+                 }
+
+                 MainActivity.loadInterstitialAd();
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        final int REQUEST_CODE = 1;
+
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE).show();
+        } else {
+            String errorMessage = String.format("There was an error initializing the YouTubePlayer (%1$s)", youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
     }
 }
